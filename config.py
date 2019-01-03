df353d4b44e0ae0768e8f23f808f7c60f7f2e873
Kevin Miller <kmiller@w99x788i.waxie.com>
2019-01-03 09:47:25 -0800

    update index.html

--- config.py
+++ config.py
@@ -2,6 +2,11 @@ from collections import namedtuple
 
 SITE_TITLE = "Jay and Jay Media"
 SITE_SUBTITLE = "Focus on your Product!"
+OWNER = {
+        "name": "Productivity in Tech", 
+        "url": "https://productivityintech.com",
+        }
+
 SITE_URL = "https://jayandjay.media"
 AUTHOR = 'KJAYMILLER'
 REGION = 'US/Pacific'
